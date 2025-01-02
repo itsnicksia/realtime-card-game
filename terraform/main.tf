bd82a26f3ecdbde7c41e2870762372f019f6cc80
Nicholas Sia <fasterthoughts@gmail.com>
2025-01-02 13:40:01 +0800

    deploy script

--- terraform/main.tf
+++ terraform/main.tf
@@ -139,6 +139,12 @@ resource "aws_iam_user" "s3_sync_user" {
   name = "s3-sync-user"
 }
 
+resource "aws_iam_policy_attachment" "s3_sync_attachment" {
+  name       = "card-game-s3-sync-attachment"
+  users      = [aws_iam_user.s3_sync_user.name]
+  policy_arn = aws_iam_policy.s3_sync_policy.arn
+}
+
 resource "aws_iam_policy" "s3_sync_policy" {
   name        = "s3-sync-policy"
   description = "Policy to allow S3 sync operations"
