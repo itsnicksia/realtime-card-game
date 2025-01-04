a7065b2ff0c0f50ecf837ab3851381d8889d07e1
Nicholas Sia <fasterthoughts@gmail.com>
2025-01-04 19:01:05 +0800

    wip

--- unity-game-renderer/Assets/Source/Services/Player.cs
+++ unity-game-renderer/Assets/Source/Services/Player.cs
@@ -7,6 +7,8 @@ namespace Source
     public class Player : NetworkBehaviour
     {
         public NetworkVariable<int> playerHp = new(1_000_000);
+
+        // TODO: NetworkSerializable list type for cards? Just do 5 cards for now.
         public NetworkVariable<int> card1 = new(-1);
 
         private void Start()
